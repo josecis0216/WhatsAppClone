b5dc1e0df42a7775ad3b627fdf1d780e81ba8d31
josecis0216 <josecis0216@gmail.com>
2024-07-25 08:56:40 -0600

    finished chat screen, and chat list screen UI

---
+++ src/screens/ChatsScreen.js
@@ -0,0 +1,14 @@
+import { View, Text, FlatList } from 'react-native';
+
+import chats from '../../assets/data/chats.json';
+import ChatListItem from '@/src/components/ChatListItem/index';
+
+
+const ChatsScreen = () => {
+    return <FlatList 
+        data={chats} 
+        renderItem={({ item }) => <ChatListItem chat={item} />}
+    />
+};
+
+export default ChatsScreen;
