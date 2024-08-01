fb74622d40888c3794f486ebdd08cecdb20dc386
josecis0216 <josecis0216@gmail.com>
2024-08-01 08:36:46 -0600

    working on new tabs

--- (tabs)/_layout.tsx
+++ (tabs)/_layout.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -19,8 +21,8 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Chats',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="message1" size={size} color={color} />
           ),
         }}
       />
@@ -34,6 +36,42 @@ export default function TabLayout() {
           href: null,
         }}
       /> */}
+      <Tabs.Screen
+        name="status"
+        options={{
+          title: 'Status',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="logo-whatsapp" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="calls"
+        options={{
+          title: 'Calls',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="logo-whatsapp" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="camera"
+        options={{
+          title: 'Camera',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="logo-whatsapp" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="logo-whatsapp" size={size} color={color} />
+          ),
+        }}
+      />
       <Tabs.Screen
         name="notImplemented"
         options={{
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -31,9 +31,8 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
-        <Stack.Screen name="index" options={{ title: 'Chats', headerShown: false }} />
-        <Stack.Screen name="chat" />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        {/* <Stack.Screen name="index" options={{ title: 'Chats', headerShown: false }} /> */}
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
